4ece11c9e06313ff35d0a54af9c71236262ce031
ManuelRopero <maroperom@correo.udistrital.edu.co>
2021-12-30 15:14:55 -0500

    Se mejoro el responsive del header y del footer, se añadio animacion de color a los logos del footer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 app.get('/',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/home.html')));
-app.post('/',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/home.html')));
+app.post('/home',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/home.html')));
 app.get('/pepe',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/registro.html')));
 app.get('/register',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/empty',(req,rest) => rest.sendFile(path.resolve(__dirname, './views/empty.html')));
